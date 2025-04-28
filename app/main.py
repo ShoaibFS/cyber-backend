8736a47567fd170a247ed0b1122d722349e94b74
Muhammad Shoaib <165574655+ShoaibFS@users.noreply.github.com>
2025-04-28 20:08:33 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 # enable CORS for only your frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL],
+    allow_origins=["*"],   # temporarily allow all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
